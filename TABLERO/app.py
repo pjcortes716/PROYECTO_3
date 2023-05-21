e971393343cc0526f3bf19fbcc223c346fc32f84
= <=>
2023-05-21 12:29:26 -0500

    callback de grafica

--- TABLERO/app.py
+++ TABLERO/app.py
@@ -4,6 +4,7 @@ import dash
 import dash
 from dash import dcc
 from dash import html
+from dash import Dash, Input, Output
 import pandas as pd
 import geopandas as gpd
 import plotly.express as px
@@ -44,20 +45,21 @@ datos_dep['DPTO']=datos_dep['DPTO'].astype('int')
 df_merged = departamentos.merge(datos_dep, left_on=['DPTO'], right_on=['DPTO'])
 #Ahora si la funcion
 def colombian_map(df,periodo,opcion):
+    dict_drop={1:'Promedio',2:"Maximo",3:'Minimo'}
     if opcion==1:#se grafica la media
         hover=['puntaje_ingles','puntaje_mate', 'puntaje_soc', 'puntaje_nat','puntaje_lec']
         labels_1={'NOMBRE_DPT':"Departamento",'puntaje_mate':'puntaje matematicas','puntaje_soc':'puntaje c.sociales',
                                'puntaje_nat':'puntaje c.naturales','puntaje_lec':'puntaje lectura'}
     elif opcion==2:#Se grafica el maximo puntaje
-        hover=['max_p_ingles','max_p_mate','max_p_soc','max_p_nat','max_p_lec''max_p_global']
+        hover=['max_p_ingles','max_p_mate','max_p_soc','max_p_nat','max_p_lec','max_p_global']
         labels_1={'NOMBRE_DPT':"Departamento",'max_p_ingles':'Máximo puntaje en ingles','max_p_soc':'Máximo puntaje c.sociales',
                                'max_p_nat':'Máximo puntaje c.naturales','max_p_lec':'Maximo puntaje letura','max_p_global':'Máximo puntaje global',
                                'max_p_mate':'Máximo puntaje matematicas'}
     elif opcion==3:
-        hover=['min_p_ingles','min_p_mate','min_p_soc','min_p_nat','min_p_lec''min_p_global']
+        hover=['min_p_ingles','min_p_mate','min_p_soc','min_p_nat','min_p_lec','min_p_global']
         labels_1={'NOMBRE_DPT':"Departamento",'min_p_ingles':'Minimo puntaje en ingles','min_p_soc':'Minimo puntaje c.sociales',
-                               'min_p_nat':'Minimo puntaje c.naturales','min_p_lec':'Minimo puntaje letura','min_p_global':'Máximo puntaje global',
-                               'min_p_mate':'Máximo puntaje matematicas'}
+                               'min_p_nat':'Minimo puntaje c.naturales','min_p_lec':'Minimo puntaje letura','min_p_global':'Minimo puntaje global',
+                               'min_p_mate':'Minimo puntaje matematicas'}
     df_mod=df[df['periodo']==periodo]
     df_mod=df_mod.set_index("NOMBRE_DPT")
     fig = px.choropleth(df_mod, geojson=df_mod.geometry, 
@@ -75,9 +77,10 @@ def colombian_map(df,periodo,opcion):
     fig.update_layout(
         margin={"r":0,"t":30,"l":10,"b":10},
         coloraxis_colorbar={
-            'title':"""Promedio del puntaje global"""})
+            'title':"""{} del puntaje global""".format(dict_drop[opcion])})
     return fig
-
+####################################################################################################################################################
+#####################################################FIN DE LA FUNCION PARA GENERAR EL MAPA#########################################################
 
 #Los periodos disponibles
 periodos = {9:'20194',0:'20142',1:'20151',4:'20162',6:'20172',10:'20201',12:'20224',5:'20171',11:'20211',7:'20181',3:'20161',2:'20152',8:'20191'}     
@@ -96,6 +99,9 @@ app.layout = html.Div([#este div es el encabezado del tablero, lo usamos para ub
     style={'width': '75%', 'display': 'inline-block','verticalAlign': 'top'}),  
         
     ]),
+    html.Div(#Este div los usamos para dejar un margen izquierdo
+        children=[],
+        style={'width': '1.5%', 'display': 'inline-block','verticalAlign': 'top'}),
     html.Div(#Este div lo usamos para ubicar la grafica del mapa
     children=[
         html.Br(style={"line-height": "40"}),
@@ -117,11 +123,11 @@ app.layout = html.Div([#este div es el encabezado del tablero, lo usamos para ub
 
     ),
     #Ubicamos un DIV para separar las dos secciones del tablero
-    html.Div(#Este div lo usamos para ubicar las listas desplegables del modelo de prediccion y la salida del mismo
+    html.Div(#
     children=[
         #NO UBICAR NADA AQUI, ES SOLO PARA DEJAR ESPACIO ENTRE EL LADO DERECHOY EL IZQUIERDO DEL TABLERO
     ],
-    style={'width': '5%', 'display': 'inline-block','verticalAlign': 'top'}),
+    style={'width': '3%', 'display': 'inline-block','verticalAlign': 'top'}),
 
 
 
@@ -131,9 +137,26 @@ app.layout = html.Div([#este div es el encabezado del tablero, lo usamos para ub
         
     ],
     style={'width': '47.5%', 'display': 'inline-block','verticalAlign': 'top',"border":"1px gray ridge"}
-    )
+    ),
+    html.Div(#Este div los usamos para dejar un margen derecho
+        children=[],
+        style={'width': '1.5%', 'display': 'inline-block','verticalAlign': 'top'})
 ])
-
+#diccionario de opciones del dropdown
+dict_drop={'Promedio':1,"Maximo":2,'Minimo':3}
+#callback de la grafica
+@app.callback(
+    Output(component_id='mapa', component_property='figure'),
+    Input(component_id='barra_periodos', component_property='value'),
+    Input(component_id='dropdown-estadistico', component_property='value')
+)
+def update_map(input_value,estadistico):
+    value=int(input_value)
+    periodo=periodos[value]
+    periodo=int(periodo)
+    opcion=dict_drop[estadistico]
+    fig=colombian_map(df_merged,periodo,opcion)
+    return fig
 
 
 if __name__ == '__main__':
